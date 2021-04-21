4c712e9f63818af17db617c94f1865d8b1a46032
Traian Alexandru <victor_traian@yahoo.com>
2021-04-21 10:35:59 -0400

    clean Profile

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,29 +5,26 @@ export default function Profile({ profile }) {
   return (
     <Table striped bordered hover>
       <thead>
-        {profile.length > 0 && (
-          <tr>
-            <th>Name</th>
-            <th>Description</th>
-            <th>Total stargazers</th>
-            <th>Total watchers</th>
-            <th>Link</th>
-          </tr>
-        )}
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Total stargazers</th>
+          <th>Total watchers</th>
+          <th>Link</th>
+        </tr>
       </thead>
       <tbody>
-        {profile.length > 0 &&
-          profile.map((row) => (
-            <tr key={row.id}>
-              <td>{row.name}</td>
-              <td>{row.description}</td>
-              <td>{row.stargazers_count}</td>
-              <td>{row.watchers_count}</td>
-              <td>
-                <a href={row.url}>{row.url}</a>
-              </td>
-            </tr>
-          ))}
+        {profile.map((row) => (
+          <tr key={row.id}>
+            <td>{row.name}</td>
+            <td>{row.description}</td>
+            <td>{row.stargazers_count}</td>
+            <td>{row.watchers_count}</td>
+            <td>
+              <a href={row.url}>{row.url}</a>
+            </td>
+          </tr>
+        ))}
       </tbody>
     </Table>
   );
