834ea65bdeba384b78740646079d34e351077a02
Traian Alexandru <victor_traian@yahoo.com>
2021-04-22 20:49:27 -0400

    app

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders serarch form', () => {
+test('renders search form', () => {
   render(<App />);
   const linkElement = screen.getByText(/Search/i);
   expect(linkElement).toBeInTheDocument();
