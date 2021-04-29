55207b15112a6e0155f606993cd9df736d839ef7
Traian Alexandru <victor_traian@yahoo.com>
2021-04-29 10:47:08 -0400

    clean

--- src/hooks/useProfile2.js
+++ src/hooks/useProfile2.js
@@ -8,7 +8,6 @@ export const FETCH_PROFILE_SUCCESS = 'FETCH_PROFILE_SUCCESS';
 export const FETCH_PROFILE_FAILURE = 'FETCH_PROFILE_FAILURE';
 export const SORT_BY_NAME = 'SORT_BY_NAME';
 export const SORT_BY_STARS = 'SORT_BY_STARS';
-export const SET_USER = 'SET_USER';
 
 const sortByProperty = (obj, param) => reverse(sortBy(obj, [param]));
 
